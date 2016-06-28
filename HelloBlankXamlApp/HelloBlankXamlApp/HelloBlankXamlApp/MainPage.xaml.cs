cf798a5a3c907171f6a7bbc771f2d2a8b8599882
李進興 Vulcan <vulcan@miniasp.com>
2016-06-28 11:35:59 +0800

    Xamarin.Forms您好

---
+++ HelloBlankXamlApp/HelloBlankXamlApp/HelloBlankXamlApp/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloBlankXamlApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnbtnOK_Clicked(object sender, EventArgs e)
+        {
+            lblSayHello.Text = $"Hello, {entYourName.Text}";
+        }
+    }
+}
