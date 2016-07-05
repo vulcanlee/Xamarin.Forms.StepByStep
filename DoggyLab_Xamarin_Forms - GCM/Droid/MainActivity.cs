fb636cd1ec9be2f16118232f170581c389dd4fb6
李進興 Vulcan <vulcan@miniasp.com>
2016-07-05 20:02:27 +0800

    Azure Mobile & GCM

---
+++ DoggyLab_Xamarin_Forms - GCM/Droid/MainActivity.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Gcm.Client;
+
+namespace DoggyLab.Droid
+{
+	[Activity (Label = "DoggyLab.Droid", 
+		Icon = "@drawable/icon", 
+		MainLauncher = true, 
+		ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
+		Theme = "@android:style/Theme.Holo.Light")]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+            // Set the current instance of MainActivity.
+            instance = this;
+
+            global::Xamarin.Forms.Forms.Init (this, bundle);
+			Microsoft.WindowsAzure.MobileServices.CurrentPlatform.Init();
+			LoadApplication (new App ());
+
+            try
+            {
+                // Check to ensure everything's setup right
+                GcmClient.CheckDevice(this);
+                GcmClient.CheckManifest(this);
+
+                // Register for push notifications
+                System.Diagnostics.Debug.WriteLine("Registering...");
+                GcmClient.Register(this, PushHandlerBroadcastReceiver.SENDER_IDS);
+            }
+            catch (Java.Net.MalformedURLException)
+            {
+                CreateAndShowDialog("There was an error creating the Mobile Service. Verify the URL", "Error");
+            }
+            catch (Exception e)
+            {
+                CreateAndShowDialog(e.Message, "Error");
+            }
+        }
+
+        private void CreateAndShowDialog(String message, String title)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+            builder.SetMessage(message);
+            builder.SetTitle(title);
+            builder.Create().Show();
+        }
+
+        // Create a new instance field for this activity.
+        static MainActivity instance = null;
+
+        // Return the current activity instance.
+        public static MainActivity CurrentActivity
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+}
+
---
+++ DoggyLab_Xamarin_Forms/DoggyLab/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DoggyLab
+{
+	public static class Constants
+	{
+		// Replace strings with your mobile services and gateway URLs.
+		public static string ApplicationURL = @"https://doggylab.azurewebsites.net";
+	}
+}
+
