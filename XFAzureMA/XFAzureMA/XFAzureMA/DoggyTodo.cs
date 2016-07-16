bc3d73ae8fc239bd311faf185dc5324aa538fb8d
李進興 Vulcan <vulcan@miniasp.com>
2016-07-16 22:10:28 +0800

    Azure Mobile後端資料庫存取

---
+++ XFAzureMA/XFAzureMA/XFAzureMA/DoggyTodo.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XFAzureMA
+{
+    public class DoggyTodo
+    {
+        string id;
+        string name;
+        bool done;
+
+        [JsonProperty(PropertyName = "id")]
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        [JsonProperty(PropertyName = "text")]
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        [JsonProperty(PropertyName = "complete")]
+        public bool Done
+        {
+            get { return done; }
+            set { done = value; }
+        }
+
+        [Version]
+        public string Version { get; set; }
+    }
+}
---
+++ XFAzureMA/XFAzureMA/XFAzureMA/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XFAzureMA
+{
+    public partial class MainPage : ContentPage
+    {
+        MobileServiceClient client;
+        IMobileServiceTable<DoggyTodo> todoTable;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            this.client = new MobileServiceClient("http://doggylab.azurewebsites.net");
+            this.todoTable = client.GetTable<DoggyTodo>();
+
+            todoList.Refreshing += TodoList_Refreshing;
+            buttonAdd.Clicked += async (s, e) =>
+            {
+               await todoTable.InsertAsync(new DoggyTodo
+                {
+                    Name = DateTime.Now.ToString()
+                });
+                await Refresh();
+            };
+            buttonDel.Clicked += async (s, e) =>
+            {
+                if(todoList.SelectedItem != null)
+                {
+                    var fooItem = (DoggyTodo)todoList.SelectedItem;
+                    await todoTable.DeleteAsync(fooItem);
+                    await Refresh();
+                }
+            };
+        }
+
+        private async void TodoList_Refreshing(object sender, EventArgs e)
+        {
+            await Refresh();
+        }
+
+        public async Task Refresh()
+        {
+
+            List<DoggyTodo> items = await todoTable.ToListAsync();
+
+            todoList.ItemsSource = items;
+
+            todoList.EndRefresh();
+        }
+    }
+}
