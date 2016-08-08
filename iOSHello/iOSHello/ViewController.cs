8050643a276929ed5513e253df7868c49b56d300
李進興 Vulcan <vulcan@miniasp.com>
2016-08-08 13:14:37 +0800

    Xamarin.iOS & Android 您好

---
+++ iOSHello/iOSHello/ViewController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using UIKit;
+
+namespace iOSHello
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            btnOK.TouchUpInside += (s, e) =>
+              {
+                  lblWelcome.Text = $"{txtYourName.Text} 您好";
+              };
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
---
+++ iOSHello/iOSHello/ViewController.designer.cs
@@ -0,0 +1,47 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace iOSHello
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnOK { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lblWelcome { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField txtYourName { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (btnOK != null) {
+                btnOK.Dispose ();
+                btnOK = null;
+            }
+
+            if (lblWelcome != null) {
+                lblWelcome.Dispose ();
+                lblWelcome = null;
+            }
+
+            if (txtYourName != null) {
+                txtYourName.Dispose ();
+                txtYourName = null;
+            }
+        }
+    }
+}
